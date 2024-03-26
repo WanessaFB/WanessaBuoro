e9510bbd5aa8680ee9760f1579a7d2cddb230238
WanessaFB <141192638+WanessaFB@users.noreply.github.com>
2024-03-26 00:41:26 -0300

    Add files via upload

---
+++ style.js
@@ -0,0 +1,22 @@
+const sobre = document.querySelector('#sobre-mim');
+const portifolio = document.querySelector('#portifolio');
+const formacao = document.querySelector('#formacao');
+const contato = document.querySelector('#contato');
+const secaoSobre = document.querySelector('#secao-sobre');
+const secaoFormacao = document.querySelector('#secao-formacao');
+const secaoContato = document.querySelector('#secao-contato');
+const secaoPortifolio = document.querySelector('#secao-portifolio');
+
+
+sobre.addEventListener('click', () => {
+  secaoSobre.scrollIntoView({ behavior: 'smooth' });
+});
+formacao.addEventListener('click', () => {
+  secaoFormacao.scrollIntoView({ behavior: 'smooth' });
+});
+contato.addEventListener('click', () => {
+  secaoContato.scrollIntoView({ behavior: 'smooth' });
+});
+portifolio.addEventListener('click', () => {
+  secaoPortifolio.scrollIntoView({ behavior: 'smooth' });
+});
